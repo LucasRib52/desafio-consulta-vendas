3eea98706bc3ac184a0d3034258034260fafef80
LucasRib52 <lucascardosoprogramador@gmail.com>
2026-03-09 22:36:03 -0300

    Finalizando desafio de consultas: Report e Summary implementados

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,29 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleSumDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query(value = "SELECT obj FROM Sale obj JOIN FETCH obj.seller " +
+            "WHERE obj.date BETWEEN :min AND :max " +
+            "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))",
+            countQuery = "SELECT count(obj) FROM Sale obj " +
+                    "WHERE obj.date BETWEEN :min AND :max " +
+                    "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Sale> searchReport(LocalDate min, LocalDate max, String name, Pageable pageable);
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSumDTO(obj.seller.name, SUM(obj.amount)) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :min AND :max " +
+            "GROUP BY obj.seller.name")
+    List<SaleSumDTO> searchSummary(LocalDate min, LocalDate max);
 }
